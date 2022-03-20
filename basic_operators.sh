7dfa8248f63293a9b76ce2c6e1afebf32a05cb3a
jeremdevs <jeremdevs@gmail.com>
2022-03-20 22:05:29 +0100

    ajout de tout mes script shell

---
+++ basic_operators.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+P=50
+B=4
+W=23
+BA=1
+TOTAL=$(($W * 3 + $B *2 + $P + $BA))
+
+
+echo "Total Cost is $TOTAL"
+
---
+++ hello_world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# script qui affiche "helle world!"
+
+echo "Hello, World!"
